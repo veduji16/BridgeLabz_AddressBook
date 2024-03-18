92cc14b67889a9a5069bb9e9f4ff8699db27067b
veduji16 <jainved01@gmail.com>
2024-03-18 23:46:00 +0530

    Master is created

---
+++ src/AddressBook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package AddressBook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("\n...Welcome to Address Book Program ...\n");
+    }
+}
